59c150ea3a454a90da27981950ff8295bfe1fab2
Luli080207 <skketchead@gmail.com>
2025-08-11 22:33:53 -0300

    fakestoreapi

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -14,3 +14,39 @@ const Register = ({setUser}) => {
     setFormData({...formData, [e.target.name]: e.target.value});
   }
 };
+
+//validaciones
+const validate = () => {
+  let tempErrors = {};
+  if (!formData.username.trim()) tempErrors.username = "Requerido";
+  if (!formData.email.trim()) {
+    tempErrors.email = "requerido";
+  } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+    tempErrors.email = "formato de correo invalido";
+  }
+  if (formData.password.length < 6) {
+    tempErrors.password = "La contraseña debe tener al menos 6 caracteres"
+  }
+  setErrors(tempErrors);
+   return Object.keys(tempErrors).length === 0;
+};
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  setErrors({});
+  setSuccess("")
+}
+
+if (!validate()) return;
+
+try {
+  const res = await fetch("https://fakestoreapi.com/users", {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({
+      email: formData.email,
+      username: formData.username,
+      password: formdata.password,
+  })
+
+});
