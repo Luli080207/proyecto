5b0b4d932fdd2324058efff215e40dbacddd9e62
Luli080207 <skketchead@gmail.com>
2025-08-12 15:59:03 -0300

    modificacion del routerapp para que maneje el estado global de usuario

--- src/router/RouterApp.jsx
+++ src/router/RouterApp.jsx
@@ -5,12 +5,18 @@ import { Login } from "../pages/Login"
 import { Register } from "../pages/Register"
 import { NotFound } from "../pages/NotFound"
 import { PrivateRoute } from "../components/PrivateRoute"
+import { useState } from "react"
 
 const RouterApp = () => {
+
+  // estado global de autenticacion
+  const [user, setUser] = useState(false)
+
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home user={user}/>} />
         <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
         <Route path="/login" element={<Login />} />
         <Route path="/registrate" element={<Register />} />
