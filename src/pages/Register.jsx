b928ecca04085c21bb8d992b41d09cb9131f16f4
Luli080207 <skketchead@gmail.com>
2025-08-11 22:12:05 -0300

    primer cambio en el register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,3 +1,16 @@
 import { useState } from "react"
 import { Layout } from "../components/Layout"
 
+const Register = ({setUser}) => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [errors, setErrors] = useState({});
+  const [succes, setSuccess] = useState("");
+
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.name]: e.target.value});
+  }
+};
